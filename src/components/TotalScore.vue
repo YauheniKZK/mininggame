8482ed15e2fe52ab72d3fa18b215ee79adcbd98d
Yauheni <yauhenikozak@gmail.com>
2024-05-07 13:40:20 +0300

    fix

--- src/components/TotalScore.vue
+++ src/components/TotalScore.vue
@@ -2,11 +2,22 @@
 import { useApplicationStore } from '@/stores/application/applicationStore';
 import { getImageUrl } from '@/utils/images';
 import { storeToRefs } from 'pinia';
-import { onMounted } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const appStore = useApplicationStore()
 const { totalScoreGetters, totalUserScoreGetter } = storeToRefs(appStore)
 // const { actionGetTotalScore } = appStore
+
+const oldValue = ref(0)
+
+watch(() => totalScoreGetters.value, (newVal, oldVal) => {
+  if (oldVal) {
+    oldValue.value = oldVal
+  }
+  if (newVal) {
+    console.log('newVal', newVal)
+  }
+})
 
 // watch(() => totalUserScoreGetter.value, async (newVal) => {
 //   if (newVal) {
@@ -26,6 +37,12 @@ onMounted(async () => {
     <img :src="getImageUrl('img/monetka1.png')" class="w-[60px] h-[60px] mr-[8px] object-contain" alt="">
     <div class="flex flex-col">
       <div class="flex items-end">
+        <n-number-animation
+          :from="oldValue"
+          :to="totalScoreGetters"
+          :active="false"
+          :precision="0"
+        />
         <span class="text-[24px] text-[#fff] font-[600] leading-[24px] mr-[6px]">{{ totalScoreGetters }}</span>
         <span class="text-[14px] text-[#fff] font-[600] leading-[18px]">{{ '$/hour' }}</span>
       </div>
