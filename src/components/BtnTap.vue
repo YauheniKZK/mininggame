8280a355fabd679546feb616c4e9686f6ab15bfc
Yauheni <yauhenikozak@gmail.com>
2024-05-12 14:18:59 +0300

    fix

--- src/components/BtnTap.vue
+++ src/components/BtnTap.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import WebApp from '@twa-dev/sdk';
+import { onMounted, onUnmounted } from 'vue';
 
 const emit = defineEmits(['startCoding'])
 const startCoding = () => {
   WebApp.HapticFeedback.impactOccurred('medium')
   emit('startCoding')
 }
+
+onMounted(() => {
+  WebApp.BackButton.show()
+})
+onUnmounted(() => {
+  WebApp.BackButton.hide()
+})
 </script>
 
 <template>
