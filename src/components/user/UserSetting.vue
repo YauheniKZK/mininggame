c7855dc670a1f33df06786620f39d485d031afff
Yauheni <yauhenikozak@gmail.com>
2024-05-13 12:57:12 +0300

    fix

--- src/components/user/UserSetting.vue
+++ src/components/user/UserSetting.vue
@@ -54,9 +54,9 @@ const updateStack = async (value: string) => {
 <template>
   <div class="flex justify-between rounded-[16px] setting-block bg-secondary p-[12px] w-full" style="box-shadow: 0px 0px 15px -3px #1d1d1d;">
     <div class="flex flex-col justify-center">
-      <span v-if="currentUserDataGetters.main_stack" class="text-[14px] text-main-color mb-[6px]">{{ $t('your_stack') }}</span>
+      <span v-if="currentUserDataGetters.main_stack" class="text-[14px] text-main-color mb-[6px]">{{ $t('your_main_stack') }}</span>
       <div v-if="currentUserDataGetters.main_stack" class="h-[20px] flex justify-center items-center rounded-[12px] p-[2px_8px] block-tag">
-        <span class="text-[16px] font-[500] leading-[24px] text-center">{{ currentUserDataGetters.main_stack }}</span>
+        <span class="text-[16px] font-[500] leading-[24px] text-center">{{ $t(currentUserDataGetters.main_stack) }}</span>
       </div>
       <span v-if="!currentUserDataGetters.main_stack" class="text-[14px] text-[#ffffff7a] w-[70%]">{{ $t('none_main_stack') }}</span>
     </div>
