d4a319bd1e0eb7eac1ff1eb162f205efa846bc98
Yauheni <yauhenikozak@gmail.com>
2024-05-15 11:44:31 +0300

    fix

--- src/pages/main/layouts/default/components/InfoBlock.vue
+++ src/pages/main/layouts/default/components/InfoBlock.vue
@@ -2,11 +2,13 @@
 import { useApplicationStore } from '@/stores/application/applicationStore';
 import { storeToRefs } from 'pinia';
 import { formatNumberWithSpaces, levels } from '@/utils/data';
-import { computed } from 'vue';
-
+import { computed, ref } from 'vue';
+import { onMounted } from 'vue';
 
 const appStore = useApplicationStore()
 const { currentUserDataGetters } = storeToRefs(appStore)
+
+const canvasBlock = ref()
 
 const balanceRef = computed(() => {
   if (currentUserDataGetters.value.balance) {
@@ -20,6 +22,44 @@ const setLevelUp = computed(() => {
 })
 const setProcent = computed(() => {
   return currentUserDataGetters.value.balance ? currentUserDataGetters.value.balance * 100 / levels[currentUserDataGetters.value.level] : 0
+})
+
+onMounted(() => {
+  if (canvasBlock.value) {
+    const ctx = canvasBlock.value.getContext('2d');
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    function getRandomNumber(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function createNumberAnimation() {
+        const number = numbers[getRandomNumber(0, numbers.length - 1)];
+        const x = getRandomNumber(0, canvasBlock.value.width);
+        let y = canvasBlock.value.height;
+
+        ctx.font = '8px "M PLUS Rounded 1c"';
+        ctx.fillStyle = 'white';
+        ctx.fillText(number, x, y);
+
+        const speed = getRandomNumber(1, 3); // Скорость движения цифры
+
+        function animate() {
+            ctx.clearRect(0, 0, canvasBlock.value.width, canvasBlock.value.height);
+            ctx.fillText(number, x, y);
+
+            if (y > 0) {
+                y -= speed;
+                requestAnimationFrame(animate);
+            }
+        }
+
+        animate();
+    }
+
+    // Запускаем анимацию каждые 500 миллисекунд
+    setInterval(createNumberAnimation, 500);
+  }
 })
 
 </script>
@@ -43,10 +83,10 @@ const setProcent = computed(() => {
         </span>
       </div>
     </div>
-    <div class="flex items-end h-auto">
+    <div class="flex items-end h-auto w-[100px]">
       <div class="h-full p-[4px] rounded-[4px] w-full flex flex-col justify-end">
-        <div class="w-full rounded-[4px] min-h-[4px] bg-[#fbdd87]" :style="`height: ${setProcent}%`">
-
+        <div class="w-full rounded-[4px] min-h-[4px] bg-[#fbdd87] relative" :style="`height: ${setProcent}%`">
+          <canvas ref="canvasBlock" width="100" height="60"></canvas>
         </div>
       </div>
     </div>
