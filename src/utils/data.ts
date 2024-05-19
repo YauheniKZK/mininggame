fc09c9f4e21ec550a13c49891840431462d9cf7c
Yauheni <yauhenikozak@gmail.com>
2024-05-19 10:43:16 +0300

    fix

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,7 +3,7 @@ export const levels: any = {
   'level_2': 10000,
   'level_3': 100000,
   'JUNIOR': 2000000,
-  'level_5': 5000000,
+  'MID_LEVEL': 5000000,
   'level_6': 10000000,
   'level_7': 50000000,
   'level_8': 100000000,
