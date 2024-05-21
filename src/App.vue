49fc2b4a856b0a8ca8f206be9ae521f35adda094
Yauheni <yauhenikozak@gmail.com>
2024-05-21 23:47:35 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -50,6 +50,13 @@ WebApp.onEvent('viewportChanged', async (event: any) => {
   }
 })
 
+const overflow = 100
+document.body.style.overflowY = 'hidden'
+document.body.style.marginTop = `${overflow}px`
+document.body.style.height = window.innerHeight + overflow + "px"
+document.body.style.paddingBottom = `${overflow}px`
+window.scrollTo(0, overflow)
+
 onBeforeUnmount(async () => {
   await actionMiningMoney(totalScoreGetters.value)
 })
