5d5bd5411a682dbf299109307c0c80e4be9c44cc
Yauheni <yauhenikozak@gmail.com>
2024-05-22 00:20:11 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -41,12 +41,12 @@ WebApp.onEvent('viewportChanged', async (event: any) => {
   }
 })
 
-const overflow = 100
-document.body.style.overflowY = 'hidden'
-document.body.style.marginTop = `${overflow}px`
-document.body.style.height = window.innerHeight + overflow + "px"
-document.body.style.paddingBottom = `${overflow}px`
-window.scrollTo(0, overflow)
+// const overflow = 100
+// document.body.style.overflowY = 'hidden'
+// document.body.style.marginTop = `${overflow}px`
+// document.body.style.height = window.innerHeight + overflow + "px"
+// document.body.style.paddingBottom = `${overflow}px`
+// window.scrollTo(0, overflow)
 
 const mainblock = ref()
 
