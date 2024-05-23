9aafe6730bb54a464910696f946e404dfbe1eea0
Yauheni <yauhenikozak@gmail.com>
2024-05-23 17:17:51 +0300

    fix

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -47,8 +47,15 @@ watch(() => successfullSyncTapClaimGetters.value, (newVal) => {
   }
 })
 
+const containerForOptions = ref()
+
 onMounted(async () => {
   console.log('222222')
+  if (containerForOptions.value) {
+    containerForOptions.value.addEventListener('touchstart', (e) => e.preventDefault(), { passive: false })
+    containerForOptions.value.addEventListener('touchmove', (e) => e.preventDefault(), { passive: false })
+  }
+
   await actionGetUser('start')
   await actionGetStacksMain()
   await actionSyncTapClaim()
@@ -139,7 +146,7 @@ onBeforeUnmount(async () => {
     <StartPage v-if="!currentUserDataGetters && !loadingGetUserGetters" />
     <Main v-if="currentUserDataGetters && !loadingGetUserGetters" />
     <Transition name="slide-up">
-      <div v-if="showModalSettingGetters" id="containerForOptions" class="z-[1] flex flex-col mb-[12px] fixed left-0 top-0 w-full h-screen bg-[#000]" >
+      <div v-if="showModalSettingGetters" ref="containerForOptions" class="z-[10] flex flex-col mb-[12px] fixed left-0 top-0 w-full h-screen bg-[#000]" >
         <div class="flex mb-[24px]">
           <span class="text-[18px] text-main-color">{{ $t('SETTING') }}</span>
         </div>
