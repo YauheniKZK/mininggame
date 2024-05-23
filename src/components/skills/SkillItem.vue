4a65e71c22f2ea540302a341c36881ab57f77d94
Yauheni <yauhenikozak@gmail.com>
2024-05-24 02:14:36 +0300

    fix

--- src/components/skills/SkillItem.vue
+++ src/components/skills/SkillItem.vue
@@ -23,7 +23,7 @@ const props = defineProps<{ title: string, profit: number }>()
         <span class="text-[#7c5b5bc5] text-[12px]">{{ 'profit/hour' }}</span>
         <span class="text-[#fff] text-[14px]">{{ props.profit + ' $' }}</span>
       </div>
-      <div class="w-full max-w-[1px] h-[32px] bg-[#63656661] ml-[12px] mr-[16px]"></div>
+      <div class="-w-[1px] h-[32px] bg-[#63656661] ml-[12px] mr-[16px]"></div>
       <div class="flex flex-col w-[calc(50%-2px)] bg-[#5d5b594f] p-[2px_8px] rounded-[4px]">
         <span class="text-[#7c5b5bc5] text-[12px] mr-[4px]">{{ 'price' }}</span>
         <span class="text-[#52f55f] text-[13px]">{{ props.profit + ' $' }}</span>
