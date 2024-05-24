d48837a3db9de2586ad4f3b65a244f57a2016336
Yauheni <yauhenikozak@gmail.com>
2024-05-24 10:57:20 +0300

    fix

--- src/assets/svg/components/CoinIcon.vue
+++ src/assets/svg/components/CoinIcon.vue
@@ -7,7 +7,7 @@
 </style>
 <g id="USDC_x2C__usd_coin">
   <g id="XMLID_2_">
-    <circle class="st1" :stroke="color" cx="16" cy="16" id="XMLID_174_" r="14.5"/>
+    <circle class="st1" cx="16" cy="16" id="XMLID_174_" r="14.5"/>
     <path :stroke="color" class="st1" d="M12.5,18.5v0.22c0,1.26,1.02,2.28,2.28,2.28h2.44c1.26,0,2.28-1.02,2.28-2.28l0,0    c0-1.02-0.67-1.91-1.65-2.19l-3.69-1.05c-0.98-0.28-1.65-1.17-1.65-2.19l0,0c0-1.26,1.02-2.28,2.28-2.28h2.44    c1.26,0,2.28,1.02,2.28,2.28v0.22" id="XMLID_72_"/>
       <line class="st1" :stroke="color" id="XMLID_3_" x1="16" x2="16" y1="23" y2="21"/><line class="st1" id="XMLID_4_" x1="16" x2="16" y1="11" y2="9"/>
       <path class="st1" :stroke="color" d="M12.5,6.11c-4.08,1.44-7,5.32-7,9.89s2.92,8.45,7,9.89" id="XMLID_7_"/>
