40c4dea672a8a9583fe99997596156149869008e
Yauheni <yauhenikozak@gmail.com>
2024-05-27 17:25:01 +0300

    fix

--- src/pages/earn/layouts/defaults/components/Tasks.vue
+++ src/pages/earn/layouts/defaults/components/Tasks.vue
@@ -1,31 +1,41 @@
 <script setup lang="ts">
 import { getImageUrl } from '@/utils/images';
-
+import { ChevronDown } from '@vicons/ionicons5'
 </script>
 
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col w-full">
     <n-collapse>
-      <n-collapse-item title="Da" name="1">
+      <n-collapse-item name="1">
+        <template #arrow>
+          <n-icon :color="'#fff'" :size="22">
+            <ChevronDown />
+          </n-icon>
+        </template>
         <template #header>
-          <span>{{ 'Daily tasks' }}</span>
+          <span class="text-[#fff] text-[18px]">{{ 'Daily tasks' }}</span>
         </template>
         <template #header-extra>
-          <span>{{ 'today: +5$' }}</span>
+          <span class="text-[#fff] text-[18px]">{{ 'today: +5$' }}</span>
         </template>
         <div class="flex">
           <span>{{ 'Tasks' }}</span>
         </div>
       </n-collapse-item>
       <n-collapse-item title="right" name="2">
+        <template #arrow>
+          <n-icon :color="'#fff'" :size="22">
+            <ChevronDown />
+          </n-icon>
+        </template>
         <template #header>
-          <span>{{ 'Main tasks' }}</span>
+          <span class="text-[#fff] text-[18px]">{{ 'Main tasks' }}</span>
         </template>
         <template #header-extra>
           <span>{{ 'no new tasks' }}</span>
         </template>
         <div class="flex">
-          <span>{{ 'Tasks' }}</span>
+          <span class="text-[#fff] text-[18px]">{{ 'Tasks' }}</span>
         </div>
       </n-collapse-item>
     </n-collapse>
