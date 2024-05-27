8e252d4784c23adce44958d5fb4204856e2f7560
Yauheni <yauhenikozak@gmail.com>
2024-05-28 00:44:16 +0300

    fix

--- src/pages/Rating.vue
+++ src/pages/Rating.vue
@@ -2,16 +2,43 @@
 import { ref } from 'vue';
 
 const currentIndexSlide = ref(0)
-const updateSlide = () => {
-  currentIndexSlide.value++
+const updateSlide = (value: number) => {
+  currentIndexSlide.value = value
 }
 </script>
 
 <template>
   <div class="flex flex-col w-full h-full px-[16px]">
-    <div class="flex justify-center items-center h-[60px] bg-[#5757579a]">
-      <span>{{ 'IMG Rating' }}</span>
-    </div>
+    <n-carousel
+      :space-between="0"
+      :loop="false"
+      slides-per-view="auto"
+      centered-slides
+      :touchable="false"
+      :current-index="currentIndexSlide"
+      :show-dots="false"
+    >
+      <n-carousel-item style="width: 100%">
+        <div class="flex justify-center items-center h-[60px] bg-[#5757579a]">
+          <span>{{ 'IMG Rating' }}</span>
+        </div>
+      </n-carousel-item>
+      <n-carousel-item style="width: 100%">
+        <div class="flex justify-center items-center h-[60px] bg-[#5757579a]">
+          <span>{{ 'IMG Rating' }}</span>
+        </div>
+      </n-carousel-item>
+      <n-carousel-item style="width: 100%">
+        <div class="flex justify-center items-center h-[60px] bg-[#5757579a]">
+          <span>{{ 'IMG Rating' }}</span>
+        </div>
+      </n-carousel-item>
+      <n-carousel-item style="width: 100%">
+        <div class="flex justify-center items-center h-[60px] bg-[#5757579a]">
+          <span>{{ 'IMG Rating' }}</span>
+        </div>
+      </n-carousel-item>
+    </n-carousel>
     <n-carousel
       :space-between="0"
       :loop="false"
@@ -22,18 +49,21 @@ const updateSlide = () => {
       :show-dots="false"
     >
       <n-carousel-item style="width: 33.333%">
-        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide">{{ 'Beginner' }}</span>
+        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide(0)">{{ 'Beginner' }}</span>
       </n-carousel-item>
       <n-carousel-item style="width: 33.333%">
-        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide">{{ 'Trainer' }}</span>
+        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide(1)">{{ 'Trainer' }}</span>
       </n-carousel-item>
       <n-carousel-item style="width: 33.333%">
-        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide">{{ 'Junior' }}</span>
+        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide(2)">{{ 'Junior' }}</span>
       </n-carousel-item>
       <n-carousel-item style="width: 33.333%">
-        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide">{{ 'Mibble' }}</span>
+        <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide(3)">{{ 'Mibble' }}</span>
       </n-carousel-item>
     </n-carousel>
+    <div class="flex flex-col">
+      {{ 'Rating' }}
+    </div>
   </div>
 </template>
 
