43404413fa38a9461536aa0096f9ca8616c89ea8
Yauheni <yauhenikozak@gmail.com>
2024-05-27 18:09:47 +0300

    fix

--- src/pages/earn/layouts/defaults/components/Tasks.vue
+++ src/pages/earn/layouts/defaults/components/Tasks.vue
@@ -3,6 +3,8 @@ import { getImageUrl } from '@/utils/images';
 import { ChevronForward } from '@vicons/ionicons5'
 import { ref } from 'vue';
 
+const showModal = ref(false)
+
 const arrayDays = ref([
   {
     label: 'Day 1',
@@ -59,6 +61,26 @@ const arrayDays = ref([
 
 <template>
   <div class="flex flex-col w-full">
+    <ModalDialog
+      :show-modal="showModal"
+      :width="'80%'"
+      :max-width="'90%'"
+      @close="showModal = false"
+    >
+      <template #content>
+        <span class="text-[22px] font-[600] text-[#fff]">
+          {{ 'Day 1' }}
+        </span>
+        <div class="flex flex-col pt-[16px]">
+          <span class="text-[#fff] font-[500] text-[22px]">{{ '1$' }}</span>
+          <div class="flex justify-center pt-[16px]">
+            <div class="min-w-[100px] h-[34px] flex justify-center items-center rounded-[4px]" style="border: 2px solid #fbdd87;">
+              <span class="text-[16px] text-[#fff]">{{ 'Claim' }}</span>
+            </div>
+          </div>
+        </div>
+      </template>
+    </ModalDialog>
     <n-collapse class="tasks-collapse">
       <n-collapse-item name="1">
         <template #arrow>
@@ -79,9 +101,12 @@ const arrayDays = ref([
             class="flex flex-col p-[8px] rounded-[8px] justify-center items-center"
             :class="day.isClaim ? 'bg-[#9d869d]' : 'bg-[#3d3d3c85]'"
             style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;width: calc(25% - 7px);"
+            @click="showModal = true"
           >
             <span class="text-[#ffffff9c] text-[12px] leading-[24px]">{{ day.label }}</span>
-            <span class="text-[#9bff00] text-[18px] leading-[24px] font-[500]">{{ day.count + '$' }}</span>
+            <span class="text-[#9bff00] text-[18px] leading-[24px] p-[4px_2px] font-[500] flex justify-center w-full bg-[#414141] rounded-[4px_4px_6px_6px]">
+              {{ day.count + '$' }}
+            </span>
           </div>
         </div>
       </n-collapse-item>
