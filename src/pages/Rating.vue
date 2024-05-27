1105fed3ef91543a8290c6f9b9c0b1cbc84d020f
Yauheni <yauhenikozak@gmail.com>
2024-05-28 00:20:06 +0300

    fix

--- src/pages/Rating.vue
+++ src/pages/Rating.vue
@@ -3,8 +3,30 @@
 </script>
 
 <template>
-  <div class="flex flex-col w-full h-full">
-    {{ 'rating' }}
+  <div class="flex flex-col w-full h-full px-[16px]">
+    <div class="flex justify-center items-center">
+      <span>{{ 'IMG Rating' }}</span>
+    </div>
+    <n-carousel
+      :space-between="30"
+      :loop="false"
+      slides-per-view="auto"
+      centered-slides
+      draggable
+    >
+      <n-carousel-item style="width: 30%">
+        <span class="text-[#fff]">{{ 'Beginner' }}</span>
+      </n-carousel-item>
+      <n-carousel-item style="width: 20%">
+        <span class="text-[#fff]">{{ 'Trainer' }}</span>
+      </n-carousel-item>
+      <n-carousel-item style="width: 30%">
+        <span class="text-[#fff]">{{ 'Junior' }}</span>
+      </n-carousel-item>
+      <n-carousel-item style="width: 40%">
+        <span class="text-[#fff]">{{ 'Mibble' }}</span>
+      </n-carousel-item>
+    </n-carousel>
   </div>
 </template>
 
