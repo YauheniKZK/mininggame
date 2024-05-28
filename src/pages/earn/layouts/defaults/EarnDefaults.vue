6d8a284ef73c0f45b36ce2aa7e8d6b5477cc5a0a
Yauheni <yauhenikozak@gmail.com>
2024-05-28 18:57:05 +0300

    fix

--- src/pages/earn/layouts/defaults/EarnDefaults.vue
+++ src/pages/earn/layouts/defaults/EarnDefaults.vue
@@ -4,7 +4,7 @@ import Tasks from './components/Tasks.vue';
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-full h-full p-[16px]">
+  <div class="flex flex-col items-center w-full h-full p-[16px_16px_60px_16px]">
     <Tasks />
   </div>
 </template>
