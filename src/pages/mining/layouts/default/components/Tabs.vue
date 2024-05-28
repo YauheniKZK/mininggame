01cd77d63af1f6cbcde76b3aa7171528f232921a
Yauheni <yauhenikozak@gmail.com>
2024-05-29 01:04:57 +0300

    fix

--- src/pages/mining/layouts/default/components/Tabs.vue
+++ src/pages/mining/layouts/default/components/Tabs.vue
@@ -132,8 +132,7 @@ const openModal = () => {
             left: 0,
             width: '100%',
             height: '100%',
-            position: 'fixed',
-            background: '#000'
+            position: 'absolute'
           }"
         />
       </div>
