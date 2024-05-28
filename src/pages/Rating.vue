a0a271782f461421b3327b6e52461e618a3ba05e
Yauheni <yauhenikozak@gmail.com>
2024-05-28 08:12:20 +0300

    fix

--- src/pages/Rating.vue
+++ src/pages/Rating.vue
@@ -35,22 +35,30 @@ const arrayRating = ref([
     >
       <n-carousel-item style="width: 100%">
         <div class="flex justify-center items-center h-[120px] bg-[#5757579a]">
-          <span>{{ 'IMG Rating' }}</span>
+          <div class="flex justify-center items-center w-[100px] min-w-[100px] h-[100px] rounded-[50%] bg-[#63636363]">
+            <span>{{ 'IMG Rating' }}</span>
+          </div>
         </div>
       </n-carousel-item>
       <n-carousel-item style="width: 100%">
         <div class="flex justify-center items-center h-[120px] bg-[#5757579a]">
-          <span>{{ 'IMG Rating' }}</span>
+          <div class="flex justify-center items-center w-[100px] min-w-[100px] h-[100px] rounded-[50%] bg-[#63636363]">
+            <span>{{ 'IMG Rating' }}</span>
+          </div>
         </div>
       </n-carousel-item>
       <n-carousel-item style="width: 100%">
         <div class="flex justify-center items-center h-[120px] bg-[#5757579a]">
-          <span>{{ 'IMG Rating' }}</span>
+          <div class="flex justify-center items-center w-[100px] min-w-[100px] h-[100px] rounded-[50%] bg-[#63636363]">
+            <span>{{ 'IMG Rating' }}</span>
+          </div>
         </div>
       </n-carousel-item>
       <n-carousel-item style="width: 100%">
         <div class="flex justify-center items-center h-[120px] bg-[#5757579a]">
-          <span>{{ 'IMG Rating' }}</span>
+          <div class="flex justify-center items-center w-[100px] min-w-[100px] h-[100px] rounded-[50%] bg-[#63636363]">
+            <span>{{ 'IMG Rating' }}</span>
+          </div>
         </div>
       </n-carousel-item>
     </n-carousel>
@@ -62,7 +70,7 @@ const arrayRating = ref([
       :current-index="currentIndexSlide"
       centered-slides
       :show-dots="false"
-      class="min-h-[60px]"
+      class="min-h-[60px] pt-[16px]"
     >
       <n-carousel-item style="width: 33.333%;height: 60px;" :style="`background: ${currentIndexSlide === 0 ? '#000' : ''};`" class="item-car">
         <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide(0)">{{ 'Beginner' }}</span>
@@ -77,11 +85,12 @@ const arrayRating = ref([
         <span class="text-[#fff] flex justify-center items-center h-full" @click="updateSlide(3)">{{ 'Mibble' }}</span>
       </n-carousel-item>
     </n-carousel>
-    <div class="flex flex-col">
+    <div class="flex flex-col pt-[16px]">
       <div
         v-for="item, index in arrayRating"
         :key="index"
-        class="flex justify-between items-center w-full mb-[8px]"
+        class="flex justify-between items-center w-full mb-[8px] p-[8px] rounded-[16px] bg-[#2f2f2f]"
+        style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;"
       >
         <div class="flex items-center">
           <div class="w-[40px] min-w-[40px] bg-[#868686b4] h-[40px] rounded-[50%] mr-[16px]">
@@ -90,13 +99,13 @@ const arrayRating = ref([
           <div class="flex flex-col">
             <div class="flex items-center">
               <span class="text-[#fff]">{{ item.name }}</span>
-              <span class="text-[#fff]">{{ item.balance }}</span>
+              <span class="text-[#fff]">{{ item.stack }}</span>
             </div>
-            <span class="text-[#fff]">{{ item.stack }}</span>
+            <span class="text-[#fff]">{{ item.balance }}</span>
           </div>
         </div>
         <div class="flex items-center">
-          <span>{{ item.rating }}</span>
+          <span class="text-[20px] text-[#fff]">{{ item.rating }}</span>
         </div>
       </div>
     </div>
