0559fdd1d0e709cdf9e5ec55d5fbb829acbd6c43
Yauheni <yauhenikozak@gmail.com>
2024-05-29 17:37:55 +0300

    fix

--- src/pages/mining/layouts/default/components/TopDescription.vue
+++ src/pages/mining/layouts/default/components/TopDescription.vue
@@ -3,22 +3,21 @@ import MiningIcon from '@/assets/svg/components/MiningIcon.vue';
 </script>
 
 <template>
- <div class="flex justify-between">
-  <div class="fleex pr-[24px]">
-    <div class="flex min-w-[4px] h-full bg-[#21ce70] mr-[8px]"></div>
-    <div class="flex flex-col">
-      <p class="text-[18px] font-[600]">
-        {{ 'Skills & Events' }}
-      </p>
-      <p>
-        {{ 'Upgrade your skills and take part in events and earn more so that everyone else is left behind you' }}
-      </p>
+  <div class="flex justify-between">
+    <div class="flex pr-[24px]">
+      <div class="flex min-w-[4px] w-[4px] h-auto bg-[#21ce70] mr-[8px]"></div>
+      <div class="flex flex-col">
+        <p class="text-[18px] font-[600]">
+          {{ 'Skills & Events' }}
+        </p>
+        <p>
+          {{ 'Upgrade your skills and take part in events and earn more so that everyone else is left behind you' }}
+        </p>
+      </div>
     </div>
-  </div>
-  <n-icon :size="70">
-    <MiningIcon :color="'#fff'" />
-    
-  </n-icon>
+    <n-icon :size="70">
+      <MiningIcon :color="'#fff'" />
+    </n-icon>
  </div>
 </template>
 
