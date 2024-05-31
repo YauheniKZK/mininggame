641c298d9951fe096d29be10291bb7d0d640b0ab
Yauheni <yauhenikozak@gmail.com>
2024-05-31 17:16:38 +0300

    fix

--- src/pages/main/layouts/default/components/DetectedUser.vue
+++ src/pages/main/layouts/default/components/DetectedUser.vue
@@ -7,7 +7,7 @@ const { currentUserDataGetters } = storeToRefs(appStore)
 </script>
 
 <template>
-  <div class="flex items-center mb-[8px] w-full">
+  <div class="flex (items-center mb-[8px] w-f)ull">
     <div class="flex items-center detected-container relative">
       <div class="flex items-center mr-[8px]">
         <div class="item-detected"></div>
@@ -16,7 +16,7 @@ const { currentUserDataGetters } = storeToRefs(appStore)
         <div class="item-detected"></div>
       </div>
       <span class="text-[#fff] text-[14px] mr-[6px]">{{ 'Detected devcat: ' }}</span>
-      <span class="text-[#fff] text-[14px]">{{ '[' + currentUserDataGetters?.first_name || 'name user' + ']' }}</span>
+      <span class="text-[#fff] text-[14px]">{{ '[' + (currentUserDataGetters?.first_name || 'name user') + ']' }}</span>
     </div>
   </div>
 </template>
