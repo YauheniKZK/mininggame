34babd7b08122deb531d438f8697d6e555c661d8
Yauheni <yauhenikozak@gmail.com>
2024-06-02 13:45:32 +0300

    fix

--- src/pages/main/layouts/default/MainDefault.vue
+++ src/pages/main/layouts/default/MainDefault.vue
@@ -9,6 +9,8 @@ import DetectedUserV2 from './components/DetectedUserV2.vue';
 import MainInfoUserV2 from './components/MainInfoUserV2.vue';
 import MainInfoUserV3 from './components/MainInfoUserV3.vue';
 
+import ProgressUser from './components/ProgressUser.vue';
+
 import InfoBalanceUser from './components/InfoBalanceUser.vue';
 import InfoBalanceUserV2 from './components/InfoBalanceUserV2.vue';
 import TapBlockV2 from './components/TapBlockV2.vue';
@@ -19,6 +21,7 @@ import TapsInfoUser from './components/TapsInfoUser.vue';
 <template>
   <div class="flex flex-col w-full p-[16px_16px_60px_16px]">
     <MainInfoUserV3 class="z-[1] mb-[16px]" />
+    <ProgressUser class="z-[1] mb-[16px]" />
     <InfoBalanceUserV2 class="z-[1] mb-[16px]" />
     <DetectedUserV2 class="z-[1] mb-[16px]" />
     <MainInfoUserV2 class="z-[1] mb-[16px]" />
