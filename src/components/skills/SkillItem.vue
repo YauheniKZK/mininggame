9ad4f4e5dea9b2e1d5ab389f41d41492cfd030a3
Yauheni <yauhenikozak@gmail.com>
2024-06-04 18:42:39 +0300

    fix

--- src/components/skills/SkillItem.vue
+++ src/components/skills/SkillItem.vue
@@ -5,20 +5,25 @@ const props = defineProps<{ title: string, profit: number }>()
 </script>
 
 <template>
-  <div class="flex flex-col p-[12px] bg-[#3d3d3c85] cursor-pointer item-skill">
-    <div class="flex mb-[8px] bg-[#332b4b] header-block relative -top-[10px] -left-[10px]">
+  <div class="flex flex-col bg-[#3d3d3c85] cursor-pointer item-skill">
+    <div class="flex bg-[#332b4b] header-block relative top-[0px] left-[0px] max-w-[90%] min-h-[40px]">
       <div class="flex flex-col">
         <span class="text-[#ffffffbe] text-[22px] leading-[20px] title">{{ props.title }}</span>
-        <span class="text-[#ffffffbe] text-[12px]">{{ 'lvl: ' + '5' }}</span>
-        
-
       </div>
     </div>
+    <div class="flex items-center p-[8px_8px_8px_0]">
+      <div class="flex w-[4px] bg-[#ff3f3f] mr-[8px] h-[20px]"></div>
+      <span class="text-[#ffffffbe] text-[12px]">{{ 'lvl: ' + '5' }}</span>
+    </div>
     <div class="flex items-center">
-      <div class="flex flex-col w-[60%]">
-        <span class="text-[#eeeeee7a] text-[12px]">{{ 'profit/hour' }}</span>
-        <span class="text-[#fff] text-[14px]">{{ props.profit + ' $' }}</span>
+      <div class="flex w-[60%]">
+        <div class="flex w-[4px] bg-[#f4c543] mr-[8px] h-auto"></div>
+        <div class="flex flex-col ">
+          <span class="text-[#eeeeee7a] text-[12px]">{{ 'profit/hour' }}</span>
+          <span class="text-[#fff] text-[14px]">{{ props.profit + ' $' }}</span>
+        </div>
       </div>
+ 
       <div class="flex flex-col flex-grow bg-[#5d5b594f] p-[2px_8px] rounded-[4px]">
         <span class="text-[#eeeeee7a] text-[12px] mr-[4px]">{{ 'price' }}</span>
         <span class="text-[#52f55f] text-[13px]">{{ props.profit + ' $' }}</span>
