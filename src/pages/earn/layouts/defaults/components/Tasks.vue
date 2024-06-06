ccb4ae39d78da91d327ff62e00d151e657998d6c
Yauheni <yauhenikozak@gmail.com>
2024-06-06 19:09:45 +0300

    fix

--- src/pages/earn/layouts/defaults/components/Tasks.vue
+++ src/pages/earn/layouts/defaults/components/Tasks.vue
@@ -3,6 +3,7 @@ import { getImageUrl } from '@/utils/images';
 import { ChevronForward } from '@vicons/ionicons5'
 import ModalDialog from '@/components/ui/ModalDialog.vue';
 import { ref } from 'vue';
+import { CheckboxOutline } from '@vicons/ionicons5';
 
 const showModal = ref(false)
 
@@ -98,28 +99,6 @@ const tasks = ref([
       </div> -->
       <img :src="getImageUrl('img/full-sunrise-cat4.png')" class="object-cover h-full absolute -right-[75px] top-0" alt="" />
     </div>
-    <ModalDialog
-      :show-modal="showModal"
-      :width="'80%'"
-      :max-width="'90%'"
-      @close="showModal = false"
-    >
-      <template #content>
-        <div class="flex flex-col items-center">
-          <img :src="getImageUrl('svg/daily_coin.svg')" class="w-[82px] min-w-[82px] h-auto object-contain mb-[16px]" alt="" />
-          <span class="text-[#fff] font-[600] mb-[8px] text-[24px]">{{ 'Day 1' }}</span>
-          <div class="flex items-center">
-            <img :src="getImageUrl('svg/coin-default.svg')" class="w-[24px] min-w-[24px] h-auto object-contain mr-[4px]" alt="" />
-            <span class="text-[#fff] font-[500] text-[22px]">{{ '1$' }}</span>
-          </div>
-          <div class="flex justify-center pt-[16px]">
-            <div class="min-w-[100px] h-[34px] flex justify-center items-center rounded-[4px]" style="border: 2px solid #fbdd87;">
-              <span class="text-[16px] text-[#fff]">{{ 'Claim' }}</span>
-            </div>
-          </div>
-        </div>
-      </template>
-    </ModalDialog>
     <n-collapse class="tasks-collapse" accordion>
       <n-collapse-item name="1">
         <template #arrow>
@@ -133,23 +112,103 @@ const tasks = ref([
         <template #header-extra>
           <span class="text-[#48ede7c0] text-[14px]">{{ 'today: +5$' }}</span>
         </template>
-        <div class="flex flex-wrap gap-[8px]">
-          <div
-            v-for="(day, index) in arrayDays"
-            :key="index"
-            class="flex flex-col p-[4px] rounded-[8px] justify-center items-center"
-            :class="day.isClaim ? 'bg-[#9d869d]' : 'bg-[#3d3d3c85]'"
-            style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;width: calc(33.333% - 6px);"
-            @click="showModal = true"
-          >
-            <span class="text-[#ffffff9c] text-[14px] leading-[38px]">{{ day.label }}</span>
-            <div class="p-[4px_2px] flex justify-center items-center w-full bg-[#414141] rounded-[4px_4px_6px_6px]">
-              <img :src="getImageUrl('svg/coin-default.svg')" class="w-[24px] min-w-[24px] h-auto object-contain mr-[4px]" alt="" />
-                <span class="text-[#9bff00] text-[16px] leading-[38px]  font-[500]">
-                {{ day.count + '$' }}
-              </span>
-            </div>
-    
+        <div class="flex justify-between">
+          <div class="flex flex-col w-[50%]">
+            <n-timeline>
+              <n-timeline-item type="success" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 1' }}</span>
+                </template>
+                <div class="flex items-center">
+                  <span class="checkinday">{{ 'Day confirmed - ' }}</span>
+                    <n-icon :color="'#f4c543'" :size="20" class="mx-[8px]">
+                    <CheckboxOutline />
+                  </n-icon>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="warning" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 2' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday mb-[4px]">{{ '+ 1$' }}</span>
+                  <button class="btn z-[1]" @click="false">
+                    <n-spin v-if="false" :size="24" :stroke="'#fff'" class="mr-[8px]" />
+                    <span class="btn__content text-[#000] font-[600] text-[12px]">{{ 'Confirm day' }}</span>
+                    <span class="btn__glitch"></span>
+                    <span class="btn__label">day</span>
+                  </button>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 3' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 2$' }}</span>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 4' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 5$' }}</span>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 5' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 10$' }}</span>
+                </div>
+              </n-timeline-item>
+            </n-timeline>
+          </div>
+          <div class="flex flex-col w-[50%]">
+            <n-timeline>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 6' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 20$' }}</span>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 7' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 50$' }}</span>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 8' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 100$' }}</span>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 9' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 500$' }}</span>
+                </div>
+              </n-timeline-item>
+              <n-timeline-item type="info" :line-type="'dashed'">
+                <template #header>
+                  <span class="checkinday-title">{{ 'Day 10' }}</span>
+                </template>
+                <div class="flex flex-col">
+                  <span class="checkinday">{{ '+ 1000$' }}</span>
+                </div>
+              </n-timeline-item>
+            </n-timeline>
           </div>
         </div>
       </n-collapse-item>
@@ -194,6 +253,14 @@ const tasks = ref([
 </template>
 
 <style scoped>
+.checkinday {
+  color: #fff;
+  font-size: 14px;
+}
+.checkinday-title {
+  color: #fff;
+  font-size: 18px;
+}
 .bg-block {
   overflow: hidden;
   padding: 16px 0 16px 16px;
@@ -211,5 +278,88 @@ const tasks = ref([
   border-radius: 50%;
   background-position: -20px -7px;
   opacity: 0.9;
+}
+
+:root {
+  --yellow-color: #fcee09; 
+  --red-color: #ff003c;
+  --black-color: #050a0e;
+  --blue-color: #00f0ff;
+  --white-color: #fafafa;
+}
+
+.btn {
+  width: 110px;
+  height: 24px;
+  border: 0;
+  outline: none;
+  background: var(--yellow-color);
+  cursor: pointer;
+  position: relative;
+  font-family: Tomorrow, sans-serif;
+  font-size: .85rem;
+  text-transform: uppercase;
+  color: var(--black-color);
+  clip-path: polygon(92% 0, 100% 25%, 100% 100%, 8% 100%, 0% 75%, 0 0);
+}
+
+.btn__content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  right: 2px;
+  bottom: 2px;
+  background: var(--yellow-color);
+  clip-path: polygon(92% 0, 100% 25%, 100% 100%, 8% 100%, 0% 75%, 0 0);
+}
+
+.btn--secondary {
+  background-color: var(--white-color);
+}
+
+.btn.red .btn__content {
+  background-color: var(--red-color);
+  color: var(--white-color);
+}
+
+.btn__label {
+  font-size: .40rem;
+  position: absolute;
+  bottom: -1px;
+  right: 8%;
+  padding: 0 5px;
+  background-color: var(--yellow-color);
+  z-index: 3;
+  border-left: 1px solid var(--blue-color);
+}
+
+.btn.red .btn__label {
+  background-color: var(--white-color);
+  color: var(--black-color);
+}
+
+.btn__glitch {
+  display: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fcf0095d;
+  filter: drop-shadow(-2px 3px #67e3f35b) drop-shadow(-1px -3px #02d7f37e) drop-shadow(2px 1px #02d7f363);
+}
+
+.btn.red .btn__glitch {
+  background-color: var(--red-color);
+}
+
+.btn .btn__glitch,
+.btn .btn__content::after,
+.btn .btn__glitch,
+.btn .btn__content::after {
+  display: block;
 }
 </style>
